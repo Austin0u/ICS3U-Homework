5668616ca1aed7478b11b7ee6788e9f339f453ce
unknown <austin.ou@student.tdsb.on.ca>
2025-02-09 14:35:50 -0500

    changed basic folder into a separate java project.

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+# Super cool heading
+This is a folder for all homework for ICS3U.
+
+Idk how the settings work so just like don't set this as your repo and don't change anything pls 😭
---
+++ src/Input.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+
+        // Prompt the user for decimal
+        System.out.print("Enter a decimal (double): ");
+        int input = (int) scanner.nextDouble(); // Read the input (double) and cast as interger
+
+        /*  
+        Alternatively, you can use the following methods to read the input as an integer:
+        
+        1. Store as double and cast as integer in new variable:
+        double input = keyboard.nextDouble(); // Read the input (double)
+        int integer = (int) input; // Cast the input as an integer
+
+        2. Store as a double and cast as an interger in the print statement:
+        double input = keyboard.nextDouble(); // Read the input (double)
+
+        System.out.println("Your input (double) as an integer is " + (int) input);
+        */ 
+        
+        // Output the user input
+        System.out.println("Your input (double) as an integer is " + input);
+        scanner.close(); 
+    }
+}
