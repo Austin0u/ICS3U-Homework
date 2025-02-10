818bdd5b2e63121e9b3f54b9a4c35a228251d2aa
unknown <austin.ou@student.tdsb.on.ca>
2025-02-10 16:59:36 -0500

    Forgot to save changes on previous commit. Now updated.

--- src/QuadraticFormula.java
+++ src/QuadraticFormula.java
@@ -1,40 +1,41 @@
+// Mathematical Ooperations Homework (2/10/25)
 import java.util.Scanner;
 
 public class QuadraticFormula {
-    
-
     private static void print(String text) {
         System.out.print(text);
 
     }
 
     public static double[] quadraticFormula(double a, double b, double c) {
-        double x1 = (-b + Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
-        double x2 = (-b - Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
-
-        return new double[]{x1, x2};
+        // Calculate each root and return
+        return new double[] { 
+            (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a), // Value for root 1
+            (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a)  // Vakye for root 2
+        };
     }
 
-    public static void main (String[] arg) {
+    public static void main(String[] arg) {
+        // Declarations
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user for inputs
+        // Prompts user for inputs
         print("\nEnter your a value: ");
         double a = scanner.nextDouble();
-        
+
         print("Enter your b value: ");
         double b = scanner.nextDouble();
 
         print("Enter your c value: ");
         double c = scanner.nextDouble();
 
-        double[] roots = quadraticFormula(a, b, c); // calculates
+        double[] roots = quadraticFormula(a, b, c); // calculates and stores as an array
 
+        // Prints the results
         print("\nGiven the eqaution: " + a + "x^2 + " + b + "x + " + c + " = 0,");
         print("\nYour first root is: " + roots[0]);
         print("\nYour second root is: " + roots[1]);
 
         scanner.close();
     }
-
 }
