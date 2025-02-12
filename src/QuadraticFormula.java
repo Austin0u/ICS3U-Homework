fc2a17a67ce1263b7bb73c96f47af284ba75e241
Austin <austin.ou@student.tdsb.on.ca>
2025-02-11 21:13:25 -0500

    Optimized quadratic formula to use discriminent

--- src/QuadraticFormula.java
+++ src/QuadraticFormula.java
@@ -8,10 +8,11 @@ public class QuadraticFormula {
     }
     
     public static double[] calculateRoots(double a, double b, double c) {
+        double discriminant = Math.pow(b, 2) - (4 * a * c); // Calculates the discriminant
         // Calculate each root and return as array
         return new double[] { 
-            (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a), // Value for root 1
-            (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a)  // Value for root 2
+            (-b + Math.sqrt(discriminant)) / (2 * a), // Value for root 1
+            (-b - Math.sqrt(discriminant)) / (2 * a)  // Value for root 2
         };
     }
 
@@ -31,14 +32,17 @@ public class QuadraticFormula {
 
         scanner.close();
 
+        // Calculations
+        double discriminant = Math.pow(b, 2) - (4 * a * c); // Calculates the discriminant
         double[] roots = calculateRoots(a, b, c); // Calculates and returns roots as an array
+        
 
         // Prints the results
         print("\nGiven the equation: " + a + "x^2 + " + b + "x + " + c + " = 0,");
 
-        if (roots[0] == roots[1]) { // One real root
+        if (discriminant==0) { // One real root
             print("\nYour only root is: " +  (-b / (2 * a))); // Value for singular root
-        } else if (!Double.isNaN(roots[0]) && !Double.isNaN(roots[1])) { // Two real roots
+        } else if (discriminant>0) { // Two real roots
             print("\nYour first root is: " + roots[0]);
             print("\nYour second root is: " + roots[1]);
         } else {
