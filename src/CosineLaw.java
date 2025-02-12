e13c2e001fc2f431a9b92eb660fe3d50dd0cd1f0
unknown <342906989@TDSB.ON.CA>
2025-02-12 11:36:54 -0500

    Added cosine law hwk, need to update comments

---
+++ src/CosineLaw.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CosineLaw {
+    
+    public static void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in); //Creates scanner object
+        
+        // Prompts user for 2 sides and an angle
+        System.out.print("Please enter the value of Side A: ");
+        double sideA = keyboard.nextDouble();
+
+        System.out.print("Please enter the value of Side B: ");
+        double sideB = keyboard.nextDouble();
+
+        System.out.print("Please enter the value of Angle C (in degrees): ");
+        double angleC = keyboard.nextDouble();
+
+        keyboard.close();
+
+        // Calculates angle 
+        double SideC = Math.sqrt(Math.pow(sideA,2)+Math.pow(sideB,2)-2*sideA*sideB*Math.cos(Math.toRadians(angleC)));
+
+        // Outputs result
+        System.out.println("The value of Side C is " + SideC);
+    }
+}
