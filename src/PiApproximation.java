f246d2040a74542b9fa59a59a99412d54849ff12
unknown <342906989@TDSB.ON.CA>
2025-02-25 11:38:55 -0500

    started loops homework

---
+++ src/PiApproximation.java
@@ -0,0 +1,18 @@
+// Loops Homework (2/25/25)
+import java.util.Scanner;
+public class PiApproximation {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        // Prompts user for an n value
+        System.out.print("Please enter an n value: ");
+        int n = keyboard.nextInt();
+
+        // Approximates pi
+        do {
+            for (int i = 0) {
+
+            }
+        } while (); // If the user says yes, do again, else break
+    }
+}
