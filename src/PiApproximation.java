0f5e75b017724789d126b29ac4112e7fb33f1544
unknown <342906989@TDSB.ON.CA>
2025-02-26 11:49:36 -0500

    modified pi approximation

--- src/PiApproximation.java
+++ src/PiApproximation.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class PiApproximation {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        double sum = 0; 
-        int n;
         char response;
-        
+
         // Loop to calculate
         do {
             // Prompts user for an n value
             System.out.print("Please enter an n value: ");
-            n = keyboard.nextInt();
-            
-            // Calculates the sum 
+            int n = keyboard.nextInt();
+            double sum = 0; // Resets sum
+
+            // Calculates the sum
             for (int i = 0; i < n; i++) {
                 sum += Math.pow(-1, i) / (2 * i + 1);
             }
 
             // Calculates and prints the approximate pi
-            double pi = 4 * sum;
-            System.out.println("The approximate value of pi is: " + pi);
+            System.out.println("The approximate value of pi is: " + 4 * sum);
 
             // Prompts user if they want to repeat the calculation
             System.out.print("Do you to try again? (y/n): ");
