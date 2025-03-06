49b7750e338dc2894ba0d91d1a7ce30c35536bc0
Austin <austin.ou@student.tdsb.on.ca>
2025-03-06 02:06:16 -0500

    Finished arrays homework #2

--- src/PolynomialEvaluator.java
+++ src/PolynomialEvaluator.java
@@ -8,9 +8,9 @@ public class PolynomialEvaluator {
         double sum = 0;
 
         // Prompts the user for the coefficients, exponents, and an x value
-        System.out.print("Please enter your coefficients: ");
+        System.out.print("Please enter your coefficients (separate with a space): ");
         String[] coefficients = keyboard.nextLine().split(" ");
-        System.out.print("Please enter your exponenets: ");
+        System.out.print("Please enter your exponents (separate with a space): ");
         String[] exponents = keyboard.nextLine().split(" ");
         System.out.print("Please enter your x value: ");
         double x = keyboard.nextDouble();
@@ -28,11 +28,11 @@ public class PolynomialEvaluator {
             boolean isNegative = (Double.parseDouble(coefficients[i]) < 0)  ? true : false;
             String term = coefficients[i] + "x^" + exponents[i];
 
-            polynomial += (isNegative) ? "-" : ((i!=0) ? "+" : "") + term;
+            polynomial += " " + ((isNegative) ? "" : ((i!=0) ? "+" : "")) + term;
         }
 
         // Outputs result
-        System.out.println(polynomial);
-        System.out.println("f(" + x + ") = "+  sum);
+        System.out.println("Your polynomial function is: " + polynomial);
+        System.out.println("You polynomial at x = " + x + " is: f(" + x + ") = "+  sum);
     }
 }
